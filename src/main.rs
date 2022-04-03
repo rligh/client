001b40dce684548dc2a2faad292fdc48f013ce83
rligh <102900674+rligh@users.noreply.github.com>
2022-04-03 20:54:03 +0800

    Connect to server and shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::net::{TcpStream, Shutdown};
+
 fn main() {
-    println!("Hello, world!");
+    let stream =
+        TcpStream::connect("101.35.253.48:19730").expect("Failed to connect to server");
+
+    stream.shutdown(Shutdown::Both).expect("Failed to shutdown");
 }
